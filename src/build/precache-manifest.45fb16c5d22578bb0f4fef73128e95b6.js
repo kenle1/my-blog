f152ddc5042fe0d0c068b535bc5383c17947e454
Ken Le <kenle456@gmail.com>
2020-03-18 01:46:47 -0400

    First commit

---
+++ src/build/precache-manifest.45fb16c5d22578bb0f4fef73128e95b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310c610bc8b15fdb75d37e2e19104944",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01717b2893e0986ad7f2",
+    "url": "/static/css/main.b04e955e.chunk.css"
+  },
+  {
+    "revision": "0aa4ed651bfa33d321a6",
+    "url": "/static/js/2.0fbc903f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.0fbc903f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01717b2893e0986ad7f2",
+    "url": "/static/js/main.fa672b85.chunk.js"
+  },
+  {
+    "revision": "f8c88a4ab800e2a20047",
+    "url": "/static/js/runtime-main.ae3b9067.js"
+  }
+]);
